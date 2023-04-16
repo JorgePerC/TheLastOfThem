cbda2bd0b4795a4c01b67c95dc8e9534b68df1f8
ParzivalStark <luisanchez859@gmail.com>
2023-04-15 18:14:05 -0600

    New code for puzzlebot with linearization (not working)

---
+++ Activites/puzzlebot_nacho/rover_control.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+PI = 3.1416
+tao = 0.001
+
+r = 0.04
+m = 1
+h = 0.15
+d = 0.10
+I = 1
+
+
+x = np.array([[  0.0  ,  0.0  ,  0.0  ]]).T
+
+K = np.array([[1, 0], [0, 2]])
+
+#theta = np.array([[r/d, -r/d]])
+
+#A = np.concatenate((D,theta),axis=0)
+
+#print(A)
+
+t0 = np.array([[0]])
+out = np.concatenate((t0,x.T), axis=1)
+
+for i in np.arange(tao,10,tao):
+    A = np.array([[r/2*np.cos(x[2,0])-h*r/d*np.sin(x[2,0]), r/2*np.cos(x[2,0])+h*r/d*np.sin(x[2,0])],
+              [r/2*np.sin(x[2,0])-h*r/d*np.cos(x[2,0]), r/2*np.sin(x[2,0])+h*r/d*np.cos(x[2,0])],
+              [r/d, -r/d]])
+    print(x.shape, A.shape)
+
+    x = x + tao*(A@x)
+
+    t0 = np.array([[i]])
+    out_dummy = np.concatenate((t0,x.T),axis=1)
+    out = np.concatenate((out, out_dummy), axis=0)
+
+df = pd.DataFrame(out, columns = ["time", "x0", "x1", "x2"])
+print(df)
+
+df.plot(x="x0", y=["x1"])
+
+plt.show()
+
