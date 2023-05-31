f360e597377afc7bc3e5e176e1c922a74e97fc1b
Jorge <jorgeperez_1@live.com>
2023-05-31 17:40:09 -0600

    completed kalman

--- capy-bot_ws/src/diff_drivetrain/scripts/kalman.py
+++ capy-bot_ws/src/diff_drivetrain/scripts/kalman.py
@@ -8,7 +8,7 @@ import tf
 
 class KalmanOdometry:
     def __init__(self, odomFreq, repsInSec = 25):
-        rospy.init_node("Kalman Odometry")
+        rospy.init_node("KalmanOdometry")
         rospy.loginfo("Starting ROSNode as Kalman odometry.")
         
         # ===== Subscribers =====
@@ -27,8 +27,8 @@ class KalmanOdometry:
 
         # ===== Rate =====
         self.rate = rospy.Rate(repsInSec)
-        self.dt = 1.0/repsInSec
-        self.dt_odom = 1.0/odomFreq
+        self.dt = 0.0
+        self.lasTime = rospy.Time.now()
         
         # ===== Shutdown =====
         rospy.on_shutdown(self.stop)
@@ -48,80 +48,86 @@ class KalmanOdometry:
             # x
             # y
             # z, but needed for multiplication
-        self.sensorVect = np.array([[0.0, 0.0, 0.0]]).T
-
-        # Aka H
-            # Identity matrix for sensor input
-            # Vector space from a virtual sensor
-            # Virtual sensor obtained from odometry
-                # x
-                # y
-                # theta
-        self.statesSensor = np.array([[1.0, 0.0, 0.0], 
-                                        [0.0, 1.0, 0.0],
-                                        [0.0, 0.0, 1.0]]) 
-
+        self.snrVectOdom = np.array([[0.0, 0.0, 0.0]]).T
         # Aka Q
             # Covariance from model noise
         self.model_Cov_Mat = np.array([ [1.0, 0.0, 0.0],
-                                [0.0, 1.0, 0.0],
-                                [0.0, 0.0, 1.0]]) 
-
-        # Aka R
-            # Covariance from sensor input
-        self.sensor_Cov_Mat = np.array([ [1.0, 0.0, 0.0], 
-                                    [0.0, 1.0, 0.0],
-                                    [0.0, 0.0, 1.0]]) 
-
+                                        [0.0, 1.0, 0.0],
+                                        [0.0, 0.0, 1.0]]) 
+        # Aka H
+            # Identity-ish matrix for sensor input
+            # Vector space from sensor readouts to statesX
+        self.statesSensor = np.array([  [1.0, 0.0, 0.0], 
+                                        [0.0, 1.0, 0.0],
+                                        [0.0, 0.0, 1.0]]) 
         # Aka P
-            # Covariance from state estimation 
+            # Covariance from state estimation
+            # This is system wide 
         self.prediction_Cov_Mat = np.array([ [0.0, 0.0, 0.0], 
-                                        [0.0, 0.0, 0.0],
-                                        [0.0, 0.0, 0.0]])
+                                            [0.0, 0.0, 0.0],
+                                            [0.0, 0.0, 0.0]])
         
         # System dynamics
-        self.A = np.array([[self.r/2*np.cos( self.xP[2,0] )-self.h*self.r/self.d*np.sin( self.xP[2,0] ),
-                            self.r/2*np.cos( self.xP[2,0] )+self.h*self.r/self.d*np.sin( self.xP[2,0] ), 0],
-                        [self.r/2*np.sin( self.xP[2,0] ) + self.h*self.r/self.d*np.cos( self.xP[2,0] ), 
-                            self.r/2*np.sin( self.xP[2,0] ) - self.h*self.r/self.d*np.cos( self.xP[2,0] ), 0],
-                        [self.r/self.d, 
-                            -self.r/self.d, 0]])
+        self.A = np.array(
+                [[self.r/2*np.cos(self.xP[2,0]) - self.h*self.r/self.d*np.sin(self.xP[2,0]),
+                        self.r/2*np.cos(self.xP[2,0]) + self.h*self.r/self.d*np.sin(self.xP[2,0]), 0],
+                [self.r/2*np.sin(self.xP[2,0]) + self.h*self.r/self.d*np.cos(self.xP[2,0]), 
+                        self.r/2*np.sin(self.xP[2,0]) - self.h*self.r/self.d*np.cos(self.xP[2,0]), 0],
+                [self.r/self.d, 
+                        -self.r/self.d, 0]])
 
 
     def runKalman(self):
-        pass
-        
-        # Recieving updates from sensors 
-        # It's done on the callbacks :D
+        # We just use this to publish the new stimation
+        msg2send = Pose2D()
 
+        msg2send.x = self.snrVectOdom[0,0]
+        msg2send.y = self.snrVectOdom[1,0]
+        msg2send.theta = self.snrVectOdom[2,0]
+        
+        self.pub_poseK.publish(msg2send)
+
+    def updateDt(self):
+        # This method should be called whenever 
+        # there's a new sensor reading
+        now = rospy.Time.now()
+        self.dt = now.secs - self.lasTime.secs
+        self.lasTime = now
 
     def get_pose(self, msg):
         # Update sensor readout
-        self.sensorVect[0,0] = msg.x
-        self.sensorVect[1,0] = msg.y
-        self.sensorVect[2,0] = msg.theta
+        self.snrVectOdom[0,0] = msg.x
+        self.snrVectOdom[1,0] = msg.y
+        self.snrVectOdom[2,0] = msg.theta
+        self.updateDt()
 
+        # Aka R
+            # Covariance from sensor input
+            # This one is only applied t
+        sensor_Cov_Mat = np.array([[0.1, 0.0, 0.0], 
+                                        [0.0, 0.1, 0.0],
+                                        [0.0, 0.0, 0.1]]) 
         # Update prediction 
-        self.xP = self.xP + self.dt_odom*( 
-            np.matmul(self.A, self.xP) +
-            # Kalman filter stuff
+        self.xP = self.xP + self.dt*( 
+            np.matmul(self.A,self.xP) +
+            # Kalman filter stuf
             np.matmul(self.prediction_Cov_Mat,
                 np.matmul(self.statesSensor.T,
-                        np.matmul(np.linalg.inv(self.sensor_Cov_Mat),
-                            np.matmul((self.sensorVect-self.statesSensor,
-                                self.xP))))))   
+                    np.matmul(
+                        np.linalg.inv(sensor_Cov_Mat),
+                        (self.snrVectOdom-np.matmul(self.statesSensor, 
+                                                    self.xP))))))
 
         # Update prediction covariance matrix
-        self.prediction_Cov_Mat = self.prediction_Cov_Mat + self.dt_odom*(
-                np.matmul(self.A, self.prediction_Cov_Mat) 
-                    + np.matmul(self.prediction_Cov_Mat, self.A.T) 
-                    + self.model_Cov_Mat -
-                        np.matmul(self.prediction_Cov_Mat,
-                        np.matmul(self.statesSensor.T,
-                            np.matmul(
-                                np.linalg.inv(self.sensor_Cov_Mat),
-                                np.matmul(self.statesSensor,
-                                        self.prediction_Cov_Mat)))))
+        self.prediction_Cov_Mat = self.prediction_Cov_Mat + self.dt*(
+                np.matmul(self.A, self.prediction_Cov_Mat) + 
+                np.matmul(self.prediction_Cov_Mat, self.A.T) +
+                self.model_Cov_Mat -
+                np.matmul(self.prediction_Cov_Mat,
+                    np.matmul(self.statesSensor.T,
+                        np.matmul(np.linalg.inv(sensor_Cov_Mat),
+                            np.matmul(self.statesSensor, 
+                                    self.prediction_Cov_Mat)))))
 
     def stop(self):
         rospy.loginfo("Ended Kalman")
